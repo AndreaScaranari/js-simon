baecacfe9c5d96ecebbfb885f2159d89ed3f623c
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-12 17:31:39 +0100

    hidden_numbers_created_prompts

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const num5 = document.querySelector(".num5");
 
 // * dichiarazione variabili
 const nums = [num1, num2, num3, num4, num5];
+const userNums = [];
+const timeoutTiming = 5000;
 
 // * dichiarazione funzioni
 // generare un numero casuale
@@ -18,15 +20,30 @@ const getRandomNumber = max => {
 
 // nascondere i numeri
 const hideNumbers = () => {
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < nums.length; i++) {
     nums[i].classList.add("d-none");
 }}
 
+// generare i prompt
+const createPrompt = () => {
+    while (userNums.length < nums.length) {
+    const userNum = parseInt(prompt("Indica un numero che ricordi"));
+    if (userNum && !userNums.includes(userNum) && !isNaN(userNum)) {
+        userNums.push(userNum);
+    }}}
+
 // * svolgimento
 // genero 5 numeri casuali
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < nums.length; i++) {
     nums[i].innerText = getRandomNumber(100);
 }
 
 // nascondo i numeri dopo 30 secondi
-const numbersTimeOut = setTimeout(hideNumbers, 30000);
+const numbersTimeOut = setTimeout(() => {
+    hideNumbers();
+}, timeoutTiming);
+
+// chiedo all'utente quali numeri si ricorda
+const promptTimeOut = setTimeout(() => {
+    createPrompt();
+}, timeoutTiming+500);
